9eae7c962ec4f149e0bfc9ce9b657eb8e11f5d8a
Marchand007 <m.marchand22@hotmail.com>
2023-06-17 22:15:40 -0400

    requis 6 fail

--- processor.test.js
+++ processor.test.js
@@ -36,3 +36,14 @@ test("return rawData in ojbect", function ()
     let result = processor("9701::<489584872710>");
     expect(result.rawData).not.toEqual(undefined);
 });
+
+//requis 6 (Requis : Retourne -1 pour « rawData » si la portion de « rawData » de la transmission ne commence pas par < et finit par >)
+test("return -1 if rawData not begin with < and finish with >", function ()
+{
+    let result1 = processor("9701::489584872710");
+    expect(result1).toEqual(-1);
+    let result2 = processor("9701::489584872710>");
+    expect(result2).toEqual(-1);
+    let result3 = processor("9701::<489584872710");
+    expect(result3).toEqual(-1);
+});
