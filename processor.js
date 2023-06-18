bac7df95759d37b7a21892bb9e86dcc3765fa487
Marchand007 <m.marchand22@hotmail.com>
2023-06-17 22:19:17 -0400

    requis 6 pass

--- processor.js
+++ processor.js
@@ -4,6 +4,14 @@ function processor(transmission) {
     }
     
     let parts = transmission.split("::");
+
+    let size = parts[1].length;
+    let first = parts[1].charAt(0);
+    let last = parts[1].charAt(size - 1);
+
+    if (first != "<" || last != ">") {
+        return -1;
+    }
     return {
         id: Number.parseInt(parts[0]),
         rawData: parts[1]
--- processor.test.js
+++ processor.test.js
@@ -8,42 +8,41 @@ describe("transmission processor", function ()
         let result = processor("9701::<489584872710>");
         expect(typeof result).toEqual("object");
     });
-});
 
-test("returns -1 if '::' not found", function ()
-{
-    let result = processor("9701<489584872710>");
-    expect(result).toEqual(-1);
-});
+    test("returns -1 if '::' not found", function ()
+    {
+        let result = processor("9701<489584872710>");
+        expect(result).toEqual(-1);
+    });
 
-test("returns id in object", function ()
-{
-    let result = processor("9701::<489584872710>");
-    expect(result.id).not.toEqual(undefined);
-});
+    test("returns id in object", function ()
+    {
+        let result = processor("9701::<489584872710>");
+        expect(result.id).not.toEqual(undefined);
+    });
 
-//requis 4 (L’id propriété doit être de type « Number »)
+    //requis 4 (L’id propriété doit être de type « Number »)
+    test("id must be type << Number >>", function ()
+    {
+        let result = processor("9701::<489584872710>");
+        expect(typeof result.id).toEqual("number");
+    });
 
-test("id must be type << Number >>", function ()
-{
-    let result = processor("9701::<489584872710>");
-    expect(typeof result.id).toEqual("number");
-});
+    //requis 5 (Requis : L’objet retourné devrait contenir une « rawData » comme propriété)
+    test("return rawData in ojbect", function ()
+    {
+        let result = processor("9701::<489584872710>");
+        expect(result.rawData).not.toEqual(undefined);
+    });
 
-//requis 5 (Requis : L’objet retourné devrait contenir une « rawData » comme propriété)
-test("return rawData in ojbect", function ()
-{
-    let result = processor("9701::<489584872710>");
-    expect(result.rawData).not.toEqual(undefined);
-});
-
-//requis 6 (Requis : Retourne -1 pour « rawData » si la portion de « rawData » de la transmission ne commence pas par < et finit par >)
-test("return -1 if rawData not begin with < and finish with >", function ()
-{
-    let result1 = processor("9701::489584872710");
-    expect(result1).toEqual(-1);
-    let result2 = processor("9701::489584872710>");
-    expect(result2).toEqual(-1);
-    let result3 = processor("9701::<489584872710");
-    expect(result3).toEqual(-1);
+    //requis 6 (Requis : Retourne -1 pour « rawData » si la portion de « rawData » de la transmission ne commence pas par < et finit par >)
+    test("return -1 if rawData not begin with < and finish with >", function ()
+    {
+        let result1 = processor("9701::489584872710");
+        expect(result1).toEqual(-1);
+        let result2 = processor("9701::489584872710>");
+        expect(result2).toEqual(-1);
+        let result3 = processor("9701::<489584872710");
+        expect(result3).toEqual(-1);
+    });
 });
